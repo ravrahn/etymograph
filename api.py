9c6f6959208b7f7bfa9515eb8164352fc5813156
Jothini Sivananthan <jothini123@live.com>
2015-09-27 20:33:36 +1000

    modfied code

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def search(): # ET-5
 
 @app.route('/<word>/roots')
 def roots(word): # ET-6
-for record in graph.cypher.execute("MATCH (n <id>)-[r:root*1..<number>]->() RETURN n"):
+for record in graph.cypher.execute("MATCH (n)-[r:root*..depth]->() WHERE id(n) = id RETURN n"):
  
 	return 'hello {}'.format(word)
 
