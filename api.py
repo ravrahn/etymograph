e8268e4a74db73b10c2173d28c7325248e45127b
Jothini <jothini123@live.com>
2015-10-02 12:09:28 +1000

    added code to my function mostly done with id and other property info

--- api.py
+++ api.py
@@ -88,6 +88,16 @@ def roots(word): # ET-6
         return 'hello {}'.format(request.args['q'])
     else:
         query = ''
+    try:
+	node = graph.node(word)
+	node.properties["id"] = word
+    except GraphError:
+	errRoot = ("errrror")
+	response = json.jsonify({'error':errNum,'description':errRoot})
+	response.status_code = 404
+	return "bad"
+
+   return str(node.properties)
    
     
 @app.route('/<word>/descs')
