41122c9c72e70c5ed344144d488248b06b90505e
Ryan Spencer <spencer.ryanjames@gmail.com>
2015-10-14 10:09:49 +1100

    Show autocomplete suggestions with anchored regex

--- static/js/etymograph.main.js
+++ static/js/etymograph.main.js
@@ -12,26 +12,25 @@ var substringMatcher = function(strs) {
   };
 };
 
-// jQuery
-
+/* start jQuery */
 $(document).ready(function(){
-
   // Because Jinja2 does not make this easy
   $("#search_field").attr("placeholder", "Look up the origins of a word...");
 
+  // Start Bloodhound search engine
   var words = new Bloodhound({
       datumTokenizer: function (datum) {
           return Bloodhound.tokenizers.whitespace(datum.value);
       },
       queryTokenizer: Bloodhound.tokenizers.whitespace,
       remote: {
-        url: '/search?q=%QUERY',
+        url: '/search?q=^%QUERY',
         wildcard: '%QUERY'
       }
   });
-
   words.initialize();
 
+  // Set options for typehead and for which form it is attached
   $("#search_field").typeahead({
 	  hint: false,
 	  highlight: true,
@@ -40,8 +39,7 @@ $(document).ready(function(){
   {
 	  name: 'words',
 	  displayKey: 'orig_form',
-	  source: words,
+      source: words.ttAdapter(),
 	  limit: 5
   });
-
 });
