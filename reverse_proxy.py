47e6bd09b527395a31179d1aba3c6981e338028b
Owen Cassidy <ravrahn@gmail.com>
2015-10-20 10:53:28 +1100

    Attempt to fix redirect loop

--- reverse_proxy.py
+++ reverse_proxy.py
@@ -5,12 +5,12 @@ class ReverseProxied(object):
     different than what is used locally.
 
     In nginx:
-    location /myprefix {
+    location / {
         proxy_pass http://192.168.0.1:5001;
         proxy_set_header Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
+        proxy_set_header X-Script-Name /;
         }
 
     :param app: the WSGI application
