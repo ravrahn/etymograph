cd17a0f17e7fa05b41c6052059dd5145dab089da
Ryan Spencer <spencer.ryanjames@gmail.com>
2015-10-21 17:52:48 +1100

    graph.js flag link for words now should link back to the origin graph.

--- static/js/graph.js
+++ static/js/graph.js
@@ -115,7 +115,7 @@ function makeGraph(roots, descs, form) {
         if (definition !== undefined){
             info += "<p> Definition: "+ definition + '</p>';
         }
-        info += '<a href="/flag/' + id + '">Flag this as incorrect?</a>';
+        info += '<a href="/flag/' + id + '?next=' + next_url + '">Flag this as incorrect?</a>';
         $(".infobar").html(info);
     });
 
