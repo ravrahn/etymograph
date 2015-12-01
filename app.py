0eaa06be12aad8a14e5c05802ae034ff3b62e9aa
Owen Cassidy <ravrahn@gmail.com>
2015-12-01 22:44:16 +1100

    Added basic error handling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from helpers import *
 import model
 from web import web
 from api import api
@@ -18,6 +19,11 @@ app.register_blueprint(user)
 app.register_blueprint(api, url_prefix='/api')
 
 app.jinja_env.globals.update(user_area=user_area)
+
+@app.errorhandler(404)
+@app.errorhandler(400)
+def error(err):
+    return render_search_template('error.html', error_code=err.code, error_message=err.description, title='Error')
 
 
 if __name__ == '__main__':
